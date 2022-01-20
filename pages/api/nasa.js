104b2276dec5fe4f46f0048b8f8843fd51d310f5
Thomas Neter <thomas@sublettr.io>
2022-01-19 21:57:14 -0500

    removed unused code

--- pages/api/nasa.js
+++
@@ -1,6 +0,0 @@
-export async function getNasa() {
-    const res = await fetch('https://api.nasa.gov/planetary/apod?api_key=YkmTmSEo7egiCG0c0fGGRNil8uBZl2CO7aIfkYdM')
-    const json = await res.json()
-  
-    return json
-}
